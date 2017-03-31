a5909699ac79f8183df2d89ed0ddb0480b06b095
tt-coder <tokio.tm@gmail.com>
2017-03-31 22:36:25 +0900

    add scan function

---
+++ script.js
@@ -0,0 +1,15 @@
+function scanJAN(){
+    var s = 'pic2shop://scan?callback='+encodeURIComponent(location.origin+location.pathname+'?r=EAN')
+    document.location = s
+}
+
+window.addEventListener('DOMContentLoaded',function(){
+    function getParameterByName(name) {
+        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+        return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+    var r = getParameterByName('r')
+    if(r) getElementById('jancode').value = r
+},false)
