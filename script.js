600bc5998e3bf00e940c483e2aa5280d8ae2724c
tt-coder <tokio.tm@gmail.com>
2017-03-31 23:19:24 +0900

    add getData function

--- script.js
+++ script.js
@@ -15,7 +15,34 @@ window.addEventListener('DOMContentLoaded',function(){
     var r = getParameterByName('r')
     if(r){
         //document.getElementById('jancode').innerHTML = r
-        var isbn = ISBN.parse(r)
-        document.getElementById('jancode').innerHTML = isbn.asIsbn10(true);
+        document.getElementById('jancode').innerHTML = r;
+        getBookData(r);
     }
 },false);
+
+function getBookData(isbn){
+    const url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn;
+    $.getJSON(url, function(data) {
+        if(!data.totalItems) {
+            $("#isbn").val("");
+            $("#BookTitle").text("");
+            $("#BookAuthor").text("");
+            $("#isbn10").text("");
+            $("#isbn13").text("");
+            $("#PublishedDate").text("");
+            $("#BookThumbnail").text("");
+            $("#BookDescription").text("");
+            $("#BookMemo").val("");
+            $("#message").html('<p class="bg-warning" id="warning">該当する書籍がありません。</p>');
+            $('#message > p').fadeOut(3000);
+        }else{
+            $("#BookTitle").text(data.items[0].volumeInfo.title);
+            $("#isbn13").text(data.items[0].volumeInfo.industryIdentifiers[0].identifier);
+            $("#isbn10").text(data.items[0].volumeInfo.industryIdentifiers[1].identifier);
+            $("#BookAuthor").text(data.items[0].volumeInfo.authors[0]);
+            $("#PublishedDate").text(data.items[0].volumeInfo.publishedDate);
+            $("#BookDescription").text(data.items[0].volumeInfo.description);
+            $("#BookThumbnail").html('<img src=\"' + data.items[0].volumeInfo.imageLinks.smallThumbnail + '\" />');
+        }
+    });
+};
