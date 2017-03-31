9f83acf249adf5ff912fc9d6c36761ebbaa32598
tt-coder <tokio.tm@gmail.com>
2017-03-31 22:53:13 +0900

    add converter

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// バーコードスキャン
 function scanJAN(){
     var s = 'pic2shop://scan?callback='+encodeURIComponent(location.origin+location.pathname+'?r=EAN')
     document.location = s
-}
+};
 
+// バーコードから番号を読み取る
 window.addEventListener('DOMContentLoaded',function(){
     function getParameterByName(name) {
         name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
@@ -11,5 +13,45 @@ window.addEventListener('DOMContentLoaded',function(){
         return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
     }
     var r = getParameterByName('r')
-    if(r) document.getElementById('jancode').innerHTML = r
-},false)
+    if(r){
+        //document.getElementById('jancode').innerHTML = r
+        var isbn = convertISBN(r)
+        document.getElementById('jancode').innerHTML = isbn
+    }
+},false);
+
+function convertISBN(id){
+  'use strict';
+  var a, b, c, i;
+  if (id.length === 13) {
+    a = 0;
+    b = 0;
+    for (i = 0; i < 6; i = i + 1) {
+      a += Number(id.charAt(i * 2));
+      b += Number(id.charAt(i * 2 + 1));
+    }
+    c = (a + b * 3) % 10;
+    if (c === 10) {
+      c = 0;
+    }
+    if (c === Number(id.charAt(12))) {
+      c = 0;
+      id = id.slice(3, 12);
+      for (i = 0; i < 10; i = i + 1) {
+        c += Number(id.charAt(i)) * (10 - i);
+      }
+      c = 11 - c % 11;
+      if (c === 10) {
+        c = 'X';
+      } else {
+        if (c === 11) {
+          c = 0;
+        } 
+      }
+      id = id + String(c);
+      return String('http://www.amazon.co.jp/dp/' + id + '/');
+    }
+    return false;
+  }
+  return false;
+};
