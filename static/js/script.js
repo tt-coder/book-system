e7d0ea96ead1a371270ab0085d9b381a66e67dbe
tt-coder <tokio.tm@gmail.com>
2017-04-05 00:12:22 +0900

    bulid test

--- static/js/script.js
+++ static/js/script.js
@@ -115,8 +115,8 @@ function getBookData(isbn){
             $("#PublishedDate").text("");
             $("#BookThumbnail").text("");
             //$("#Publisher").text("");
-            alert("書籍が見つかりませんでした。\nISBNを確認してください。");
             //document.getElementById("property").style.display = "none";
+            alert("書籍が見つかりませんでした。\nISBNを確認してください。");
         }else{ // 書籍が見つかったとき
             title = data.items[0].volumeInfo.title;
             $("#BookTitle").html(data.items[0].volumeInfo.title);
