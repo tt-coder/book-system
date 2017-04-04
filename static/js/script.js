6913d77fe62b06d6bd53a613ca7da8084e7b2275
tt-coder <tokio.tm@gmail.com>
2017-04-04 23:37:53 +0900

    fix property display

--- static/js/script.js
+++ static/js/script.js
@@ -114,7 +114,8 @@ function getBookData(isbn){
             $("#BookAuthor").text("");
             $("#PublishedDate").text("");
             $("#BookThumbnail").text("");
-            alert("書籍が見つかりませんでした。\nISBNを確認してください。")
+            alert("書籍が見つかりませんでした。\nISBNを確認してください。");
+            document.getElementById("property").style.display = "none";
         }else{ // 書籍が見つかったとき
             title = data.items[0].volumeInfo.title;
             $("#BookTitle").html(data.items[0].volumeInfo.title);
