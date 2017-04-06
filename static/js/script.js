635ccabe4b98e7b2c068461b400d7329cd683db8
tt-coder <tokio.tm@gmail.com>
2017-04-06 18:46:18 +0900

    test

--- static/js/script.js
+++ static/js/script.js
@@ -106,12 +106,8 @@ function getBookData(isbn){
         type: "GET",
         dataType: "xml",
         timeout: 10000,
-        beforeSend : function( xhr ){
-            if (window.navigator.userAgent.toLowerCase().indexOf('safari') != -1)
-                xhr.setRequestHeader("If-Modified-Since", new Date().toUTCString());
-        },
         error:function(errorThrown) {
-            alert(errorThrown);
+            //alert("エラー");
         },
         success:function(xml){
             var item = $(xml).find("item");
